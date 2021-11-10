3f6d35f43b02b54c3f3842d2507082e554eb8272
bartosz <bartosz.zablocki1@gmail.com>
2021-11-10 20:41:28 +0100

    Init Spring Boot project

---
+++ src/main/kotlin/com/zablocki/bankingapp/BankingAppApplication.kt
@@ -0,0 +1,11 @@
+package com.zablocki.bankingapp
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BankingAppApplication
+
+fun main(args: Array<String>) {
+	runApplication<BankingAppApplication>(*args)
+}
